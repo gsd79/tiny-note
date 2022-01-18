ab03c34d5f8752fc1aad9883159b14eafc096262
Grace <cgracesharpe@gmail.com>
2022-01-17 21:21:30 -0500

    spotify player rework

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -1,61 +1,13 @@
-(function() {
-    
-    function login(callback) {
-        var CLIENT_ID = '30952d37a31e45e8a0275ab430706e20';
-        var REDIRECT_URI = 'http://127.0.0.1:5500/index.html';
-        function getLoginURL(scopes) {
-            return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
-              '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
-              '&scope=' + encodeURIComponent(scopes.join(' ')) +
-              '&response_type=token';
-        }
-        
-        var url = getLoginURL([
-            'user-read-email'
-        ]);
-        
-        var width = 450,
-            height = 730,
-            left = (screen.width / 2) - (width / 2),
-            top = (screen.height / 2) - (height / 2);
-    
-        window.addEventListener("message", function(event) {
-            var hash = JSON.parse(event.data);
-            if (hash.type == 'access_token') {
-                callback(hash.access_token);
-            }
-        }, false);
-        
-        var w = window.open(url,
-                            'Spotify',
-                            'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
-                           );
-        
-    }
+player.addListener('initialization_error', ({ message }) => { 
+    console.error(message);
+});
 
-    function getUserData(accessToken) {
-        return $.ajax({
-            url: 'https://api.spotify.com/v1/me',
-            headers: {
-               'Authorization': 'Bearer ' + accessToken
-            }
-        });
-    }
+player.addListener('authentication_error', ({ message }) => {
+    console.error(message);
+});
 
-    var templateSource = document.getElementById('result-template').innerHTML,
-        template = Handlebars.compile(templateSource),
-        resultsPlaceholder = document.getElementById('result'),
-        loginButton = document.getElementById('btn-login');
-    
-    loginButton.addEventListener('click', function() {
-        login(function(accessToken) {
-            getUserData(accessToken)
-                .then(function(response) {
-                    loginButton.style.display = 'none';
-                    resultsPlaceholder.innerHTML = template(response);
-                });
-            });
-    });
-    
-})();
+player.addListener('account_error', ({ message }) => {
+    console.error(message);
+});
 
+player.connect();
